8e1fc067fb471c53b61400bd358c2824cf5507c5
= <=>
2013-12-04 17:33:13 +0000

    Add options menu, code cleanup, and misc. bugfixes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// ECMAScript 5 Strict Mode
+"use strict";
+
 // Canvas vars
 var can;
 var ctx;
@@ -21,21 +24,21 @@ function initialize() {
 	cwidth = can.width;
 	cheight = can.height;
 	
-	// Check if the browser can handle the awesomeness of canvas.
-	if (can.getContext){
+	// Check if the browser can handle the awesomeness of canvas
+	if (can.getContext) {
 		// Good browser!
 		ctx = can.getContext("2d");
 		state = "menu";
 		menu = "main";
 		ctx.font = "normal 12pt Calibri";
 		can.addEventListener("mousedown", mouseclick, false);
-		//ctx.fillStyle = "rgb(200,0,0)";
-		//ctx.fillRect(16, 16, cwidth - 16, cheight - 16);
 		gameloop();
 	}
-	else{
+	else {
 		// Bad browser!
-		// TODO: Handle unsupported browsers
+		var par = document.createElement("p");
+		par.appendChild(document.createTextNode("I'm sorry, but your web browser is too stupid to view this page."));
+		document.getElementsByTagName("body")[0].appendChild(par);
 	}
 }
 
@@ -51,13 +54,13 @@ function gameloop() {
 	// Game main loop
 	
 	ctx.clearRect(0, 0, cwidth, cheight);
-	if (state == "menu"){
+	if (state === "menu") {
 		menuloop();
 	}
-	else if (state == "map"){
+	else if (state === "map") {
 		// TODO
 	}
-	else if (state == "battle"){
+	else if (state === "battle") {
 		// TODO
 	}
 	window.requestAF(function() {
@@ -70,11 +73,17 @@ function menuloop() {
 	buttonsxpos = [];
 	buttonsypos = [];
 	buttonsaction = [];
-	if (menu == "main"){
+	if (menu === "main") {
 		ctx.fillStyle = "rgb(0,0,200)";
 		ctx.fillText("Main Menu!", 16, 16);
 		addbutton("Play", "startgame");
 		addbutton("Options", "optionsmenu");
+	}
+	else if (menu === "options") {
+		// TODO: Sliders and checkboxes
+		ctx.fillStyle = "rgb(0,0,200)";
+		ctx.fillText("Options Menu", 16, 16);
+		addbutton("Save", "saveoptions");
 	}
 }
 
@@ -85,8 +94,8 @@ function addbutton(text, action) {
 	
 	// Adds button positions to array for onclick detection
 	buttonsxpos.push(8);
-	buttonsypos.push(bpos)
-	buttonsaction.push(action)
+	buttonsypos.push(bpos);
+	buttonsaction.push(action);
 	
 	// Draw text on button
 	ctx.fillStyle = "rgb(0,200,0)";
@@ -96,15 +105,23 @@ function addbutton(text, action) {
 }
 
 function mouseclick(ev) {
-	if (state == "menu"){
+	if (state === "menu"){
 		console.log("MouseX: " + ev.pageX + ", MouseY: " + ev.pageY);
 		// Check if a button was clicked
 		buttonsxpos.forEach(function(xpos, index) {
 			if (ev.pageX >= xpos + can.offsetLeft && ev.pageX <= xpos + 128 + can.offsetLeft) {
 				if (ev.pageY >= buttonsypos[index] + can.offsetTop && ev.pageY <= buttonsypos[index] + 24 + can.offsetTop) {
-					window.alert(buttonsaction[index]);
+					//window.alert(buttonsaction[index]);
+					if (buttonsaction[index] === "optionsmenu") { 
+						menu = "options";
+					}
+					else if (buttonsaction[index] === "saveoptions") {
+						window.alert("Saved! (Not really, I haven't implemented that yet.)");
+						menu = "main";
+					}
 				}
 			}
 		});
 	}
 }
+window.onload = initialize;
